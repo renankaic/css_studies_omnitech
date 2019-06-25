48cc26b8b32416930bdfdef91433ff33966c7f53
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-06-25 11:15:23 -0300

    Adding "X" while menu is open

--- scripts/script.js
+++ scripts/script.js
@@ -37,22 +37,21 @@ $menu.addEventListener('click', toggleMenu, false);
 let isOpen = false;
 function toggleMenu() {
 
-    if (isOpen === false){
-        $navBar.classList.add('menu-opened');   
+    if (!isOpen){
+        $navBar.classList.add('menu-opened');
+        $menu.firstElementChild.classList.add('close-btn');
         isOpen = true;     
     } else {
         $navBar.classList.remove('menu-opened');
+        $menu.firstElementChild.classList.remove('close-btn');
         isOpen = false;
     }
 
 }
 
 $navBar.addEventListener('click', navClick, false)
-
 function navClick(evt) {
-
     if (evt.target.tagName == 'A'){
         toggleMenu();
     }
-
 }
